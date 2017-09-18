febe4b5971f50cc93a152d8e8a009de683d1c4a7
Albert Factora <albert.factora@gmail.com>
2017-09-18 10:05:15 -0700

    Updated mLab PORT

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
-
+const PORT = process.env.PORT || 3000;
 // Use morgan and body parser with our app
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
@@ -200,6 +200,6 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
 });
